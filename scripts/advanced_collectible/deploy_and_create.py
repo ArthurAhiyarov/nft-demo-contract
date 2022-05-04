02ac61d0817a48dde40cae70a7ef2e4ede9e001d
Arthur Ahiyarov <93097573+ArthurAhiyarov@users.noreply.github.com>
2022-05-05 00:51:20 +0500

    Update deploy_and_create.py

--- scripts/advanced_collectible/deploy_and_create.py
+++ scripts/advanced_collectible/deploy_and_create.py
@@ -9,9 +9,6 @@ from brownie import AdvancedCollectible, network, config
 
 def deploy_and_create():
     account = get_account()
-    # We want to be able to use the deployed contracts if we are on a testnet
-    # Otherwise, we want to deploy some mocks and use those
-    # Rinkeby
     advanced_collectible = AdvancedCollectible.deploy(
         get_contract("vrf_coordinator"),
         get_contract("link_token"),
